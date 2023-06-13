95dfb6f432c33ece6739feac33d29b9a7231f41f
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-13 17:49:51 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moca/views/sociodemographic_sceen.dart';
 import 'firebase_options.dart';
-import 'views/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,8 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SocioDemographicScreen(),
+      home: const SocioDemographicScreen(),
     );
   }
 }
-
