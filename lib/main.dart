f1c25dd34d6b536cf43f615fc80471f0eadf7639
ahmaddioxide <ahmadmahmood296@gmail.com>
2023-06-13 19:02:43 +0500

    cognitive_failure.dart screen crated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:moca/views/cognitive_failure.dart';
 import 'package:moca/views/sociodemographic_sceen.dart';
 import 'firebase_options.dart';
 import 'views/home_screen.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SocioDemographicScreen(),
+      home: SurveyScreen(),
     );
   }
 }
