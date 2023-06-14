6c1721b1c7033c2b10336f8f63cfa0343370510f
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-14 16:04:49 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:moca/views/Symptoms_initial_illness_screen%20.dart';
-
+import 'package:moca/views/sociodemographic_sceen.dart';
 import 'firebase_options.dart';
-import 'views/covidexperience_sceen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SymptomsInitialIllnessScreen(),
+      home: const SocioDemographicScreen(),
     );
   }
 }
