f5b5deecf662423cf72525da26afb2fd37e0be10
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-14 16:56:41 +0500

    Update covidexperience_controller.dart

--- lib/controllers/covidexperience_controller.dart
+++ lib/controllers/covidexperience_controller.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:moca/controllers/firebase_const.dart';
 
 class CovidExperienceController extends GetxController {
   String pcrTest = '';
@@ -12,17 +12,23 @@ class CovidExperienceController extends GetxController {
   String first4weeks = '';
   String week4Experience = '';
 
-  void submitForm() async {
+  void submitForm({
+    pcrTest,
+    covid,
+    result,
+    duration,
+    first4weeks,
+    week4Experience,
+    pcrTestDetails,
+  }) async {
     debugPrint('submitForm() called');
-    final User? user = FirebaseAuth.instance.currentUser;
 
-    final DocumentReference userRef = FirebaseFirestore.instance
+    await FirebaseFirestore.instance
         .collection('users')
         .doc('test_user')
         .collection('covid_experience')
-        .doc();
-
-    final Map<String, dynamic> formData = {
+        .doc(currentUser!.uid)
+        .set({
       'pcrTest': pcrTest,
       'covid': covid,
       'results': result,
@@ -30,23 +36,22 @@ class CovidExperienceController extends GetxController {
       'first4weeks': first4weeks,
       'week4Experience': week4Experience,
       'pcrTestDetails': pcrTestDetails,
-    };
+      'id': currentUser!.uid,
+    });
 
-    await userRef.set(formData);
-
-    Get.dialog(
-      AlertDialog(
-        title: const Text('Form Submitted'),
-        content: const Text('Your Covid Experience data has been submitted.'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Get.back();
-            },
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
+    // Get.dialog(
+    //   AlertDialog(
+    //     title: const Text('Form Submitted'),
+    //     content: const Text('Your Covid Experience data has been submitted.'),
+    //     actions: [
+    //       TextButton(
+    //         onPressed: () {
+    //           Get.back();
+    //         },
+    //         child: const Text('OK'),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
