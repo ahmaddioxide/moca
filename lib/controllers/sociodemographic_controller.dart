7bfe19e7bd0e3fd889bb2599e29c49d1a4b32489
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-14 16:04:44 +0500

    Update sociodemographic_controller.dart

--- lib/controllers/sociodemographic_controller.dart
+++ lib/controllers/sociodemographic_controller.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:moca/controllers/firebase_const.dart';
 
 class DemographicController extends GetxController {
+  String email = '';
+  String password = '';
   String name = '';
   String gender = '';
   String age = '18-20 years';
@@ -11,40 +14,61 @@ class DemographicController extends GetxController {
   String education = 'Matriculation (grade 9 and 10)';
   String profession = '';
 
-  void submitForm() async {
-    debugPrint('submitForm() called');
-    final User? user = FirebaseAuth.instance.currentUser;
+  /////signup
+  Future<UserCredential?> loginMethod({email, password}) async {
+    UserCredential? userCredential;
+    try {
+      userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      debugPrint('LoginCalled() called Error "$e"');
+    }
+    return userCredential;
+  }
 
-    final DocumentReference userRef = FirebaseFirestore.instance
+  void submitForm(
+      {name,
+      gender,
+      age,
+      residence,
+      education,
+      profession,
+      password,
+      email}) async {
+    debugPrint('submitForm() called');
+
+    await FirebaseFirestore.instance
         .collection('users')
         .doc('test_user')
         .collection('socio_demographic')
-        .doc();
-
-    final Map<String, dynamic> formData = {
+        .doc(currentUser!.uid)
+        .set({
       'name': name,
       'gender': gender,
       'age': age,
       'residence': residence,
       'education': education,
       'profession': profession,
-    };
+      'password': password,
+      'email': email,
+      'id': currentUser!.uid,
+    });
 
-    await userRef.set(formData);
-
-    Get.dialog(
-      AlertDialog(
-        title: const Text('Form Submitted'),
-        content: const Text('Your socio-demographic data has been submitted.'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Get.back();
-            },
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
+    // Get.dialog(
+    //   AlertDialog(
+    //     title: const Text('Form Submitted'),
+    //     content: const Text('Your socio-demographic data has been submitted.'),
+    //     actions: [
+    //       TextButton(
+    //         onPressed: () {
+    //           Get.back();
+    //         },
+    //         child: const Text('OK'),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
