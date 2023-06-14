5aceddf765b1bcca163bbd3959ac07bdfcd74141
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-14 16:56:39 +0500

    Update medicalhistory_controller.dart

--- lib/controllers/medicalhistory_controller.dart
+++ lib/controllers/medicalhistory_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:moca/controllers/firebase_const.dart';
