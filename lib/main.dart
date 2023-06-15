cb5981637ee27e081100140425bd9070542eb2e6
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-15 16:31:18 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:moca/views/sociodemographic_sceen.dart';
+import 'package:moca/views/symptoms_initial_illness_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SocioDemographicScreen(),
+      home: const SymptomsInitialIllnessScreen(),
     );
   }
 }
