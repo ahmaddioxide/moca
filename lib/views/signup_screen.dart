e5e29b0e10a23e7a7d188eb9f3c7a727759ae2bc
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-18 22:47:52 +0500

    Update signup_screen.dart

--- lib/views/signup_screen.dart
+++ lib/views/signup_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:moca/views/sociodemographic_sceen.dart';
 import '../controllers/signup_controller.dart';
+import 'login_screen.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -22,6 +23,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
         title: const Text('SignUP'),
       ),
       body: Padding(
+<<<<<<< Updated upstream
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
@@ -46,6 +48,251 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     }
                     return null;
                   },
+=======
+        padding:
+            EdgeInsets.symmetric(horizontal: 16.0, vertical: Get.height * 0.07),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              const Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple,
+                ),
+              ),
+              Image(
+                image: const AssetImage('assets/illustrations/Signup.png'),
+                height: Get.height * 0.3,
+                width: Get.width * 0.6,
+                fit: BoxFit.cover,
+              ),
+              Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          // hintText: 'Enter Your Name',
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: Colors.deepPurple,
+                          ),
+                          labelText: 'Enter Your Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(
+                            () {
+                              _controller.name = value;
+                            },
+                          );
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Your Name';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: Get.height * 0.02),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: Colors.deepPurple,
+                          ),
+                          labelText: 'Enter Your Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _controller.email = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Your Email';
+                          } else if (!value.contains('@')) {
+                            return 'Please Enter Valid Email';
+                          } else if (!value.contains('.')) {
+                            return 'Please Enter Valid Email';
+                          } else if (value.contains(' ')) {
+                            return 'Please Enter Valid Email';
+                          } else if (value.contains('..')) {
+                            return 'Please Enter Valid Email';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: Get.height * 0.02),
+                      TextFormField(
+                        obscureText: !_isPasswordVisible,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.fingerprint_outlined,
+                            color: Colors.deepPurple,
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.deepPurple,
+                            ),
+                            onPressed: () {
+                              setState(
+                                () {
+                                  _isPasswordVisible = !_isPasswordVisible;
+                                },
+                              );
+                            },
+                          ),
+                          labelText: 'Enter Your Password',
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _controller.password = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter password';
+                          } else if (value.length < 6) {
+                            return 'Password must be at least 6 characters!';
+                          } else if (value.contains(' ')) {
+                            return 'Password must not contain space!';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: Get.height * 0.02),
+                      TextFormField(
+                        obscureText: !_isRepasswordVisible,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.fingerprint_outlined,
+                            color: Colors.deepPurple,
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isRepasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.deepPurple,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isRepasswordVisible = !_isRepasswordVisible;
+                              });
+                            },
+                          ),
+                          labelText: 'Re-Enter Your Password',
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _controller.repassword = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter password';
+                          } else if (_controller.repassword !=
+                              _controller.password) {
+                            return 'Password MisMatched';
+                          } else if (value.length < 6) {
+                            return 'Password must be at least 6 characters!';
+                          } else if (value.contains(' ')) {
+                            return 'Password must not contain space!';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: Get.height * 0.02),
+                      SizedBox(
+                        width: double.infinity,
+                        height: Get.height * 0.06,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              _controller
+                                  .signup(
+                                name: _controller.name.trim(),
+                                password: _controller.password.trim(),
+                                email: _controller.email.trim(),
+                              )
+                                  .then((value) {
+                                if (value == true) {
+                                  Get.offAll(
+                                    () => const SocioDemographicScreen(),
+                                  );
+                                }
+                              });
+                            }
+                          },
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Already have an account?",
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Get.offAll(() =>
+                                  const LogInScreen()); // Navigate to the LoginScreen
+                            },
+                            child: const Text(
+                              "Log in",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.deepPurple,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+>>>>>>> Stashed changes
                 ),
                 ///////Email
                 const SizedBox(height: 16),
