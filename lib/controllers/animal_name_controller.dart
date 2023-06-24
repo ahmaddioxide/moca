62997848aa105aa1773ec770aaba3d54aa859045
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-06-24 23:20:38 +0500

    Create animal_name_controller.dart

---
+++ lib/controllers/animal_name_controller.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:moca/controllers/firebase_const.dart';
+
+class AnimalNameTestController extends GetxController {
+  int score = 0;
+
+  Future<bool> submitForm({score}) async {
+    debugPrint('submitForm() called');
+    try {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser!.uid)
+          .collection('animal_name_test')
+          .doc('data')
+          .set({
+        'score': score,
+        'id': currentUser!.uid,
+      });
+      return true;
+    } catch (e) {
+      debugPrint('Error: $e');
+      Get.snackbar(
+        'Error',
+        'Error: $e',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+      return false;
+    }
+  }
+
+  Future<bool> signup({score}) async {
+    try {
+      debugPrint('submitForm() called');
+      bool? check = await submitForm(
+        score: score,
+      );
+      if (check == false) {
+        debugPrint('ERROR is storing data!');
+        return false;
+      }
+    } catch (e) {
+      debugPrint('Error: $e');
+      Get.snackbar(
+        'Error',
+        'Error: $e',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+      return false;
+    }
+    return true;
+  }
+}
