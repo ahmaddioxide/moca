df83ca2c0d1a78013043c294a06aa9e3d18121b5
hammadalive7 <200862@students.au.edu.pk>
2023-06-26 23:29:55 +0500

    Added moca voice related tests

--- lib/views/test/sentence_repetition.dart
+++ lib/views/test/sentence_repetition.dart
@@ -14,21 +14,24 @@ class SentenceRepetitionScreen extends StatefulWidget {
 class SentenceRepetitionScreenState extends State<SentenceRepetitionScreen> {
   SpeechToText speechToText = SpeechToText();
   FlutterTts flutterTts = FlutterTts();
-  var isListening = false;
-  var score = 0;
+  bool isListening = false;
+  int score = 0;
   var sentences = [
     'I only know that John is the one to help today',
     'The cat always hides under the couch when dogs are in the room',
   ];
-  var currentSentenceIndex = 0;
-  var recognizedText = '';
+  int currentSentenceIndex = 0;
+  String recognizedText = '';
+  bool isMicEnabled = false;
+  bool secondsentence = true;
 
   @override
   void initState() {
     super.initState();
     initializeSpeechToText();
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 3), () {
       _speakSentence();
+      disableMicButton();
     });
   }
 
@@ -39,90 +42,127 @@ class SentenceRepetitionScreenState extends State<SentenceRepetitionScreen> {
     }
   }
 
+  void disableMicButton() {
+    setState(() {
+      isMicEnabled = false;
+    });
+  }
+
+  void enableMicButton() {
+    setState(() {
+      isMicEnabled = true;
+    });
+  }
+
   Future<void> _speakSentence() async {
     await flutterTts.speak(sentences[currentSentenceIndex]);
+    Future.delayed(const Duration(seconds: 3), () {
+      enableMicButton();
+    });
   }
 
   void goToNextSentence() {
     setState(() {
       recognizedText = '';
-      if (currentSentenceIndex < sentences.length - 1) {
-        currentSentenceIndex++;
-      } else {
-        currentSentenceIndex = 0;
+      currentSentenceIndex++;
+      if(secondsentence){
+        _speakSentence();
+        disableMicButton();
+        secondsentence = false;
+      }else{
+        _showNextSentencePopup();
       }
-      _speakSentence();
+
+
     });
+  }
+
+  void _showNextSentencePopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Test Completed'),
+          content: const Text('You have completed the Sentence Repetition test.'),
+
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          AvatarGlow(
-            animate: isListening,
-            endRadius: 75,
-            duration: const Duration(milliseconds: 2000),
-            glowColor: Colors.deepPurple,
-            repeatPauseDuration: const Duration(milliseconds: 100),
-            repeat: true,
-            showTwoGlows: true,
-            child: GestureDetector(
-              onTapDown: (details) async {
-                if (!isListening) {
-                  bool available = await speechToText.initialize();
-                  if (available) {
-                    setState(() {
-                      isListening = true;
-                      recognizedText = '';
-                      speechToText.listen(
-                        onResult: (result) {
-                          if (result.finalResult) {
-                            var spokenSentence = result.recognizedWords.trim();
-                            var targetSentence = sentences[currentSentenceIndex];
-                            if (spokenSentence.toLowerCase() == targetSentence.toLowerCase()) {
-                              setState(() {
-                                score++;
-                              });
-                            }
-                          } else {
-                            setState(() {
-                              recognizedText = result.recognizedWords;
-                            });
-                          }
-                        },
-                      );
-                    });
-                  } else {
-                    debugPrint('Speech recognition is not available');
-                  }
-                }
-              },
-              onTapUp: (details) {
+      floatingActionButton: AvatarGlow(
+        animate: isListening,
+        endRadius: 75,
+        duration: const Duration(milliseconds: 2000),
+        glowColor: Colors.deepPurple,
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        showTwoGlows: true,
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening && isMicEnabled) {
+              bool available = await speechToText.initialize();
+              if (available) {
                 setState(() {
-                  isListening = false;
+                  isListening = true;
+                  recognizedText = '';
+                  speechToText.listen(
+                    onResult: (result) {
+                      if (result.finalResult) {
+                        var spokenSentence = result.recognizedWords.trim();
+                        var targetSentence = sentences[currentSentenceIndex];
+                        if (spokenSentence.toLowerCase() ==
+                            targetSentence.toLowerCase()) {
+                          setState(() {
+                            score++;
+                          });
+                        }
+                        Future.delayed(const Duration(seconds: 3), (){
+                          goToNextSentence();
+                        });
+
+                      } else {
+                        setState(() {
+                          recognizedText = result.recognizedWords;
+                        });
+                      }
+
+
+                    },
+                  );
                 });
-                speechToText.stop();
-              },
-              child: CircleAvatar(
-                backgroundColor: Colors.deepPurple,
-                radius: 30,
-                child: Icon(
-                  isListening ? Icons.mic : Icons.mic_none,
-                  color: Colors.white,
-                ),
-              ),
+              } else {
+                debugPrint('Speech recognition is not available');
+              }
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          },
+          child: CircleAvatar(
+            backgroundColor: isMicEnabled ? Colors.deepPurple : Colors.grey,
+            radius: 30,
+            child: Icon(
+              isListening ? Icons.mic : Icons.mic_none,
+              color: Colors.white,
             ),
           ),
-          FloatingActionButton(
-            onPressed: goToNextSentence,
-            child: const Icon(Icons.arrow_forward),
-          ),
-
-        ],
+        ),
       ),
       appBar: AppBar(
         title: const Text('Sentence Repetition Test'),
@@ -154,5 +194,3 @@ class SentenceRepetitionScreenState extends State<SentenceRepetitionScreen> {
     );
   }
 }
-
-
