5907637ebd6987fcab7fadbf99d520c330c1afe2
ahmaddioxide <ahmadmahmood296@gmail.com>
2023-07-08 11:30:48 +0500

    added: SF to Clock Game

--- lib/controllers/clock_test_controller.dart
+++ lib/controllers/clock_test_controller.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class ClockTestController extends GetxController {
   DateTime? selectedTime;
@@ -17,6 +19,10 @@ class ClockTestController extends GetxController {
   }
 
   Future<void> saveScoreToFirestore() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('clockGameScore', score.value);
+    prefs.setInt('nextGame', 2);
+    debugPrint('Score of clockGameScore saved to SharedPreferences: ${score.value}');
     final User? currentUser = FirebaseAuth.instance.currentUser;
 
     if (currentUser != null) {
