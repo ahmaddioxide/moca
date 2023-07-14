062ced237969e88642c0b813042c1bb7b938785d
Yahya Bin Naveed <yahya.bin.naveed@gmail.com>
2023-07-15 01:01:32 +0500

    search by email works. Csv downloading broken. Cause unknown.

--- lib/controllers/admin_controller.dart
+++ lib/controllers/admin_controller.dart
@@ -1,15 +1,40 @@
 import 'dart:core';
-
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cr_file_saver/file_saver.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class AdminController extends GetxController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   RxInt _totalSubmissionsCount = RxInt(0);
+
   Rx<int> get totalSubmissionCount => _totalSubmissionsCount;
   RxBool _isTotalSubmissionLoaded = RxBool(false);
+
   Rx<bool> get isTotalSubmissionLoaded => _isTotalSubmissionLoaded;
+  TextEditingController userSearchBar = TextEditingController();
+  RxBool _isUserSearchFound = RxBool(false);
+
+  RxBool get isUserSearchFound => _isUserSearchFound;
+  RxBool _userSearchIsLoading = RxBool(false);
+
+  RxBool get userSearchIsLoading => _userSearchIsLoading;
+  RxString _searchedUserName = RxString('notFound');
+
+  Rx<String> get searchedUserName => _searchedUserName;
+  RxInt _searchedUserTotalScore = RxInt(0);
+
+  Rx<int> get searchedUserTotalScore => _searchedUserTotalScore;
+  RxString _searchedUserEmail = RxString('notFound');
+
+  Rx<String> get searchedUserEmail => _searchedUserEmail;
+
+  QuerySnapshot? _userDoc;
 
 
   Future<String> convertDataToCSV() async {
@@ -20,7 +45,7 @@ class AdminController extends GetxController {
 
     csvData.add(headers);
 
-    const String csvString = "" ;
+    const String csvString = "";
     return csvString;
   }
 
@@ -30,9 +55,9 @@ class AdminController extends GetxController {
 
   Future<void> getTotalSubmissions() async {
     final QuerySnapshot querySnapshot =
-          await FirebaseFirestore.instance.collection('users').get();
+    await _firestore.collection('users').get();
 
-    for(var doc in querySnapshot.docs) {
+    for (var doc in querySnapshot.docs) {
       debugPrint("${doc.id}");
     }
     _totalSubmissionsCount = querySnapshot.docs.length.obs;
@@ -41,4 +66,116 @@ class AdminController extends GetxController {
     debugPrint("${querySnapshot.docs.length}");
     //debugPrint("${_isTotalSubmissionLoaded}");
   }
+
+  Future<void> getUserByEmail() async {
+    _userSearchIsLoading.value = true;
+    update();
+    final searchedEmail = userSearchBar.text;
+    debugPrint(searchedEmail);
+    try {
+      final QuerySnapshot querySnapshot = await _firestore.collection('users')
+          .where('user_info.email', isEqualTo: searchedEmail)
+          .get();
+
+      if (querySnapshot == null) {
+        querySnapshot.printError();
+        debugPrint('userEmailQuery is null or undefined');
+        _isUserSearchFound.value = false;
+      }
+      else if (querySnapshot.docs.isNotEmpty) {
+        debugPrint(
+            "${querySnapshot.docs.last['user_info']['name']} ${querySnapshot
+                .docs.last['user_info']['email']} ${querySnapshot.docs
+                .last['user_info']['totalScore']}");
+        _searchedUserName.value =
+        (querySnapshot.docs.last['user_info']['name'] as String);
+        _searchedUserEmail.value =
+        (querySnapshot.docs.last['user_info']['email'] as String);
+        _searchedUserTotalScore.value =
+        (querySnapshot.docs.last['user_info']['totalScore'] as int);
+        _isUserSearchFound.value = true;
+        _userDoc = querySnapshot;
+      }
+    } finally {
+      _userSearchIsLoading.value = false;
+    }
+
+    update();
+  }
+
+  Future<void> downloadSearchedUserCsv() async {
+    if (_userDoc == null) {
+      debugPrint("_UserDoc is undefined. Search a user first!");
+      return;
+    }
+    else {
+
+      //final statusTry = await Permission.manageExternalStorage.request();
+      //Permission.storage.request();
+      //if (statusTry.isDenied ||  statusTry.isPermanentlyDenied ||  statusTry.isRestricted) {  throw "Please allow storage permission to upload files";  }
+      /*var status = await Permission.manageExternalStorage.request();
+*/
+      Map<Permission, PermissionStatus> status = await [
+        Permission.storage,
+        Permission.manageExternalStorage,
+      ].request();
+
+      //final permStatus = await Permission.storage.status;
+
+      if (status[Permission.storage]!.isDenied) {
+        Permission.storage.request();
+        debugPrint('Storage Permission denied');
+        //openAppSettings();
+      }
+      if(status[Permission.storage]!.isPermanentlyDenied) {
+        debugPrint('Storage Permission permanently denied');
+
+        openAppSettings();
+      }
+      else if(status[Permission.storage]!.isGranted){
+
+      List<List<dynamic>> rows = [];
+      List<dynamic> row = [];
+      row.add ("user_info.name");
+      row.add("user_info.email");
+      row.add("user_info.totalScore");
+      row.add("socio_demographic.age");
+      row.add("socio_demographic.education");
+      row.add("socio_demographic.gender");
+      row.add("socio_demographic.profession");
+      row.add("socio_demographic.residence");
+      row.add("medical_history.diet");
+      row.add("medical_history.medicalCondition");
+      row.add("medical_history.physicalActivity");
+      row.add("medical_history.smoke");
+      row.add("covid_experience.covid");
+      row.add("covid_experience.duration");
+      row.add("covid_experience.first4weeks");
+      row.add("covid_experience.pcrTest");
+      row.add("covid_experience.pcrTestDetails");
+      row.add("covid_experience.results");
+      row.add("covid_experience.week4Experience");
+
+      rows.add(row);
+
+      List<dynamic> userData = [];
+
+      for (String key in row) {
+        userData.add(_userDoc?.docs.last[key]);
+      }
+      rows.add(userData);
+      String csvData = const ListToCsvConverter().convert(rows);
+      String? dir = (await getApplicationDocumentsDirectory()).path;
+      if (dir != null) {
+        final filePath = '$dir/MOCA_${_userDoc?.docs
+            .last["user_info.email"]}.csv';
+        debugPrint(filePath);
+        // await CRFileSaver.requestWriteExternalStoragePermission();
+        File file = File(filePath);
+        await (await file.writeAsString(csvData)).open();
+        await file.open();
+
+      }
+    }
+  }}
 }
--- lib/views/admin_home_screen.dart
+++ lib/views/admin_home_screen.dart
@@ -6,7 +6,9 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AdminHomeScreen extends StatelessWidget {
   final AdminController _adminController = Get.put(AdminController());
+
   AdminHomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,16 +16,16 @@ class AdminHomeScreen extends StatelessWidget {
         backgroundColor: const Color(0xfff3edf7),
         title: const Center(
             child: Text(
-              'MOCA - Admin',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w400,
-                color: Color(0xFF1C1B1F),
-              ),
-            )),
+          'MOCA - Admin',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w400,
+            color: Color(0xFF1C1B1F),
+          ),
+        )),
       ),
       body: SingleChildScrollView(
-        padding:  const EdgeInsets.fromLTRB(18,40,18,18),
+        padding: const EdgeInsets.fromLTRB(18, 40, 18, 18),
         child: Column(
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -34,8 +36,7 @@ class AdminHomeScreen extends StatelessWidget {
               shadowColor: Colors.black,
               color: const Color(0xFFFFFBFE),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16)
-              ),
+                  borderRadius: BorderRadius.circular(16)),
               child: Container(
                 padding: const EdgeInsets.fromLTRB(0, 18, 0, 18),
                 width: double.infinity,
@@ -47,31 +48,31 @@ class AdminHomeScreen extends StatelessWidget {
                     Text(
                       'Total Submissions',
                       textAlign: TextAlign.center,
-                      style: GoogleFonts.roboto (
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
+                      style: GoogleFonts.roboto(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                     FutureBuilder<void>(
-                      future: _adminController.getTotalSubmissions(),
-                      builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-                        return Obx(() {
-                          if(!_adminController.isTotalSubmissionLoaded.value) {
-                            return const CircularProgressIndicator();
-                          }
-                          else {
-                            return Text(
-                              '${_adminController.totalSubmissionCount.value}',
-                              textAlign: TextAlign.center,
-                              style: GoogleFonts.roboto (
-                                fontSize: 32,
-                                fontWeight: FontWeight.w300,
-                              ),
-                            );
-                          }
-                        });
-                      }
-                    )
+                        future: _adminController.getTotalSubmissions(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<void> snapshot) {
+                          return Obx(() {
+                            if (!_adminController
+                                .isTotalSubmissionLoaded.value) {
+                              return const CircularProgressIndicator();
+                            } else {
+                              return Text(
+                                '${_adminController.totalSubmissionCount.value}',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.roboto(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              );
+                            }
+                          });
+                        })
                   ],
                 ),
               ),
@@ -84,8 +85,7 @@ class AdminHomeScreen extends StatelessWidget {
               shadowColor: Colors.black,
               color: const Color(0xFFFFFBFE),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)
-              ),
+                  borderRadius: BorderRadius.circular(16)),
               child: Container(
                 padding: const EdgeInsets.fromLTRB(0, 18, 0, 18),
                 width: double.infinity,
@@ -94,7 +94,7 @@ class AdminHomeScreen extends StatelessWidget {
                     Text(
                       textAlign: TextAlign.center,
                       'Average Score',
-                      style: GoogleFonts.roboto (
+                      style: GoogleFonts.roboto(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
                       ),
@@ -102,7 +102,7 @@ class AdminHomeScreen extends StatelessWidget {
                     Text(
                       '85',
                       textAlign: TextAlign.center,
-                      style: GoogleFonts.roboto (
+                      style: GoogleFonts.roboto(
                         fontSize: 32,
                         fontWeight: FontWeight.w300,
                       ),
@@ -119,8 +119,7 @@ class AdminHomeScreen extends StatelessWidget {
               shadowColor: Colors.black,
               color: const Color(0xFFFFFBFE),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)
-              ),
+                  borderRadius: BorderRadius.circular(16)),
               child: Container(
                 padding: const EdgeInsets.fromLTRB(0, 18, 0, 18),
                 width: double.infinity,
@@ -132,95 +131,10 @@ class AdminHomeScreen extends StatelessWidget {
                     Text(
                       textAlign: TextAlign.center,
                       'Download Data CSV',
-                      style: GoogleFonts.roboto (
+                      style: GoogleFonts.roboto(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 14,
-                    ),
-                    TextButton(
-                        onPressed: _adminController.downloadCSV,
-                        style: TextButton.styleFrom(
-                        padding: const EdgeInsets.fromLTRB(24, 10, 24, 10),
-                        backgroundColor: const Color(0xFF6750a4),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25))
-                      ),
-                        child: Text (
-                          textAlign: TextAlign.center,
-                          'Download',
-                          style: GoogleFonts.roboto (
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 17,
-            ),
-            Card(
-              elevation: 3,
-              shadowColor: Colors.black,
-              color: const Color(0xFFFFFBFE),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)
-              ),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(0, 18, 0, 18),
-                width: double.infinity,
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(30, 25, 30, 30),
-                      child: SearchBar(
-                        hintText: "Search User by Email",
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                            const EdgeInsets.symmetric(horizontal: 12)),
-                        leading: const Icon(Icons.person_outline, size: 24),
-                        trailing: const [Icon(Icons.search, size: 24)],
-                        elevation: MaterialStateProperty.all(1),
-                      ),
-                    ),
-
-                    Column(
-                      children: [
-                        Text (
-                          textAlign: TextAlign.left,
-                          'User Name: Yahya Bin Naveed',
-                          style: GoogleFonts.roboto (
-                            color: Colors.black87,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text (
-                          textAlign: TextAlign.left,
-                          'Email: yahya.bin.naveed@gmail.com',
-                          style: GoogleFonts.roboto (
-                            color: Colors.black87,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text (
-                          textAlign: TextAlign.left,
-                          'Total Score: 96',
-                          style: GoogleFonts.roboto (
-                            color: Colors.black87,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
                     ),
                     const SizedBox(
                       height: 14,
@@ -230,12 +144,12 @@ class AdminHomeScreen extends StatelessWidget {
                       style: TextButton.styleFrom(
                           padding: const EdgeInsets.fromLTRB(24, 10, 24, 10),
                           backgroundColor: const Color(0xFF6750a4),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25))
-                      ),
-                      child: Text (
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25))),
+                      child: Text(
                         textAlign: TextAlign.center,
                         'Download',
-                        style: GoogleFonts.roboto (
+                        style: GoogleFonts.roboto(
                           color: Colors.white,
                           fontSize: 14,
                           fontWeight: FontWeight.w500,
@@ -245,12 +159,114 @@ class AdminHomeScreen extends StatelessWidget {
                   ],
                 ),
               ),
-            )
+            ),
+            const SizedBox(
+              height: 17,
+            ),
+            Obx(() {
+              return Card(
+                elevation: 3,
+                shadowColor: Colors.black,
+                color: const Color(0xFFFFFBFE),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(0, 18, 0, 18),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 25, 30, 30),
+                        child: SearchBar(
+                          hintText: "Search User by Email",
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.symmetric(horizontal: 12)),
+                          leading: const Icon(Icons.person_outline, size: 24),
+                          trailing:  [IconButton(onPressed: () async {await _adminController.getUserByEmail();}, icon: const Icon(Icons.search, size: 24,))],
+                          elevation: MaterialStateProperty.all(1),
+                          controller: _adminController.userSearchBar,
+                        ),
+                      ),
+                      if (_adminController.userSearchIsLoading.value)...[
+                        const CircularProgressIndicator(),
+                      ]
+                      else if (!_adminController.userSearchIsLoading.value &&
+                          _adminController.isUserSearchFound.value)...[
+                        Column(
+                          children: [
+                            Text(
+                              textAlign: TextAlign.left,
+                              'User Name: ${_adminController.searchedUserName.value}',
+                              style: GoogleFonts.roboto(
+                                color: Colors.black87,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              textAlign: TextAlign.left,
+                              'Email: ${_adminController.searchedUserEmail.value}',
+                              style: GoogleFonts.roboto(
+                                color: Colors.black87,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              textAlign: TextAlign.left,
+                              'Total Score: ${_adminController.searchedUserTotalScore.value}',
+                              style: GoogleFonts.roboto(
+                                color: Colors.black87,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ]
+                      else if (!_adminController.userSearchIsLoading.value &&
+                          !_adminController.isUserSearchFound.value)... [
+                        Text(
+                          textAlign: TextAlign.left,
+                          'User Not Found',
+                          style: GoogleFonts.roboto(
+                            color: Colors.black87,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                      const SizedBox(
+                        height: 14,
+                      ),
+                      TextButton(
+                        onPressed:() async { _adminController.downloadSearchedUserCsv(); },
+                        style: TextButton.styleFrom(
+                            padding: const EdgeInsets.fromLTRB(24, 10, 24, 10),
+                            backgroundColor: const Color(0xFF6750a4),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25))),
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          'Download',
+                          style: GoogleFonts.roboto(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            })
           ],
         ),
       ),
     );
   }
 }
-
-
