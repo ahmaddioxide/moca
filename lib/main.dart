5f0ad634a66b4827bc4a0078e890023d40cf12be
ahmaddioxide <ahmadmahmood296@gmail.com>
2023-07-14 15:37:46 +0500

    added: Drawing Screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:moca/views/animal_name_screen.dart';
 import 'package:moca/views/signup_screen.dart';
 import 'package:moca/views/sociodemographic_sceen.dart';
+import 'package:moca/views/test/cube_3d_draw_test_screen.dart';
 import 'package:moca/views/test/visuospatial_clock_test_screen.dart';
 import 'package:moca/views/test_main_screens.dart';
 import 'firebase_options.dart';
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:   ClockTestScreen(),
+      home:    DrawingScreen(),
     );
   }
 }
