0c82baa024673dcd671fb52b7b5ae4fdb319c624
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-08-24 16:02:24 +0500

    Update cube_drawing_test_screen.dart

--- lib/views/test/cube_drawing_test_screen.dart
+++ lib/views/test/cube_drawing_test_screen.dart
@@ -10,6 +10,7 @@ import 'package:image/image.dart' as img;
 import 'package:tflite_flutter/tflite_flutter.dart';
 
 import '../../controllers/cube_drawing_controller.dart';
+
 class Classifier {
   /// Instance of Interpreter
   late Interpreter _interpreter;
@@ -34,7 +35,7 @@ class Classifier {
   /// Gets the interpreter instance
   Interpreter get interpreter => _interpreter;
 
-   predict(img.Image image) async {
+  predict(img.Image image) async {
     img.Image resizedImage = img.copyResize(image, width: 150, height: 150);
 
     // Convert the resized image to a 1D Float32List.
@@ -55,10 +56,8 @@ class Classifier {
     interpreter.run(input, output);
     print(output.toString());
     return output;
-
   }
 }
-
 
 class DrawingScreen extends StatefulWidget {
   const DrawingScreen({Key? key}) : super(key: key);
@@ -78,17 +77,15 @@ class _DrawingScreenState extends State<DrawingScreen> {
   }
 
   String processResult(List<dynamic> outputs) {
-    if(outputs[0][0]==1)
-    {
+    if (outputs[0][0] == 1) {
       _cubeController.saveScore(0);
       return 'Not A Cube';
-    }
-    else
-    {
+    } else {
       _cubeController.saveScore(1);
       return 'Cube';
     }
   }
+
   void _addPoint(Offset? point) {
     if (point != null) {
       setState(() {
@@ -103,7 +100,6 @@ class _DrawingScreenState extends State<DrawingScreen> {
     });
   }
 
-
   void _saveDrawing() async {
     if (_points.isNotEmpty) {
       initializeModel();
@@ -111,8 +107,9 @@ class _DrawingScreenState extends State<DrawingScreen> {
         ui.PictureRecorder recorder = ui.PictureRecorder();
         double canvasWidth = context.size!.width;
         double canvasHeight = context.size!.height;
-        double borderWidth = 20.0; // Adjust this value as needed for the size of the border
-        double drawingWidth = canvasWidth ;
+        double borderWidth =
+            20.0; // Adjust this value as needed for the size of the border
+        double drawingWidth = canvasWidth;
         double drawingHeight = canvasHeight;
 
         // Create a new image with a white background
@@ -147,22 +144,24 @@ class _DrawingScreenState extends State<DrawingScreen> {
           drawingHeight.toInt(),
         );
 
-        ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+        ByteData? byteData =
+            await image.toByteData(format: ui.ImageByteFormat.png);
 
         if (byteData != null) {
           if (!kIsWeb) {
             final directory = await getTemporaryDirectory();
-            final imagePath = '${directory.path}/drawing.png'; // Change the extension to .png
+            final imagePath =
+                '${directory.path}/drawing.png'; // Change the extension to .png
             final File file = File(imagePath);
             await file.writeAsBytes(byteData.buffer.asUint8List());
 
-            final imageFile=File(imagePath);
-            var results= await classifier.predict(
+            final imageFile = File(imagePath);
+            var results = await classifier.predict(
               img.decodeImage(imageFile.readAsBytesSync())!,
             );
             print(results.toString());
             processResult(results);
-            Get.to(()=> ClockTestScreen());
+            Get.to(() => ClockTestScreen());
             // Navigator.push(
             //   context,
             //   MaterialPageRoute(
@@ -173,7 +172,6 @@ class _DrawingScreenState extends State<DrawingScreen> {
             // If you are using this for web, you can convert ByteData directly to Uint8List
             final Uint8List bytes = byteData.buffer.asUint8List();
             const filePath = 'drawing.png';
-
           }
         }
       } catch (e) {
@@ -186,23 +184,28 @@ class _DrawingScreenState extends State<DrawingScreen> {
     final ui.PictureRecorder recorder = ui.PictureRecorder();
     final Canvas canvas = Canvas(recorder);
     final Paint paint = Paint()..color = color;
-    canvas.drawRect(Rect.fromLTWH(0, 0, width.toDouble(), height.toDouble()), paint);
+    canvas.drawRect(
+        Rect.fromLTWH(0, 0, width.toDouble(), height.toDouble()), paint);
     return await recorder.endRecording().toImage(width, height);
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Drawing Test', style: TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.bold, fontSize: 20),),
+        title: const Text(
+          'Drawing Test',
+          style: TextStyle(
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold,
+              fontSize: 20),
+        ),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Padding(
-            padding: EdgeInsets.only(top: 5, right: 16,left: 16,bottom: 16),
+            padding: EdgeInsets.only(top: 5, right: 16, left: 16, bottom: 16),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -210,7 +213,9 @@ class _DrawingScreenState extends State<DrawingScreen> {
                   'Draw a Square Below',
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                Text("60",),
+                Text(
+                  "60",
+                ),
               ],
             ),
           ),
@@ -218,9 +223,9 @@ class _DrawingScreenState extends State<DrawingScreen> {
             child: GestureDetector(
               onPanUpdate: (DragUpdateDetails details) {
                 RenderBox referenceBox =
-                context.findRenderObject() as RenderBox;
+                    context.findRenderObject() as RenderBox;
                 Offset localPosition =
-                referenceBox.globalToLocal(details.globalPosition);
+                    referenceBox.globalToLocal(details.globalPosition);
                 _addPoint(localPosition);
               },
               onPanEnd: (DragEndDetails details) => _addPoint(null),
@@ -240,18 +245,18 @@ class _DrawingScreenState extends State<DrawingScreen> {
             FloatingActionButton(
               heroTag: 'btn1',
               onPressed: _clearPoints,
-              child: Icon(Icons.clear),
+              child: const Icon(Icons.clear),
             ),
             const SizedBox(height: 20.0),
             FloatingActionButton(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
-
               ),
               backgroundColor: Colors.deepPurple,
               heroTag: 'btn2',
               onPressed: _saveDrawing,
-              child: const Icon(Icons.arrow_forward, color: Colors.white, size: 30.0),
+              child: const Icon(Icons.arrow_forward,
+                  color: Colors.white, size: 30.0),
             ),
           ],
         ),
@@ -280,7 +285,3 @@ class DrawingPainter extends CustomPainter {
   @override
   bool shouldRepaint(DrawingPainter oldDelegate) => true;
 }
-
-
-
-
