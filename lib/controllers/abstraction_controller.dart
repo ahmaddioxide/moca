29e318d95c2e6d1bab83d9f26e7a697d51994a66
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-08-24 16:02:49 +0500

    Update abstraction_controller.dart

--- lib/controllers/abstraction_controller.dart
+++ lib/controllers/abstraction_controller.dart
@@ -3,27 +3,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:moca/views/test/delayRecall_test_screen.dart';
-import 'package:moca/views/test/orientation_screen.dart';
 
 class AbstractionController extends GetxController {
   Future<void> submitTaskResults(
-      List<bool> taskResults,
-      ) async {
+    List<bool> taskResults,
+  ) async {
     final User? currentUser = FirebaseAuth.instance.currentUser;
     if (currentUser != null) {
       // Save the results to Firestore
       final CollectionReference userCollection =
-      FirebaseFirestore.instance.collection('users');
+          FirebaseFirestore.instance.collection('users');
       final DocumentReference userDoc = userCollection.doc(currentUser.uid);
       // final CollectionReference resultsCollection =
       // userDoc.collection('abstraction_results');
 
-      await userDoc.update({'abstraction_results':{'taskResults': taskResults}}).then((value) {
+      await userDoc.update({
+        'abstraction_results': {'taskResults': taskResults}
+      }).then((value) {
         Get.snackbar('Success', "Task results saved successfully.",
             backgroundColor: Colors.green,
             colorText: Colors.white,
             snackPosition: SnackPosition.BOTTOM);
-        Get.offAll(() =>  const DelayRecallScreen());
+        Get.offAll(() => const DelayRecallScreen());
       }, onError: (error) {
         Get.snackbar('Error', "Task results could not be saved.",
             backgroundColor: Colors.red,
