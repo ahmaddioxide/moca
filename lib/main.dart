9ad3f389840a32388a6b5a1f32c4615cfc284ba6
Car6on <89093185+chmuhammadasim@users.noreply.github.com>
2023-08-25 10:54:42 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: user != null ? const OnBoardingPage() : const SignUpScreen(),
       home: const OnBoardingPage(),
+      //home: const MainTestScreen(),
     );
   }
 }
