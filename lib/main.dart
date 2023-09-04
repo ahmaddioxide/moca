ab49a3c7897a03f589c69b8b43c6989556a47027
hammadalive7 <200862@students.au.edu.pk>
2023-09-04 19:15:50 +0500

    Fixed Draw Cube Screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     //
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CogWheel',
+      title: 'CogQuest',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
